eb19c56c63055adc9234adbdb9a0787ec6eb8e3b
MZainAsif <kingorootmodapk@gmail.com>
2023-02-26 23:19:58 +0500

    first

---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:food_panda_ui/constats/color.dart';
+import 'package:food_panda_ui/constats/constants.dart';
+import 'package:food_panda_ui/dashboard/splash_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      // theme: ThemeData.dark(),
+      theme: ThemeData(
+          primarySwatch: MyColors.navy,
+          brightness: Brightness.light,
+          scaffoldBackgroundColor: const Color(0xfffcfcfc),
+          appBarTheme: const AppBarTheme(
+              backgroundColor: Colors.white,
+              elevation: .2,
+              titleTextStyle:
+                  TextStyle(fontFamily: Bold, color: Color(0xff323232)),
+              iconTheme: IconThemeData(color: MyColors.primaryColor))),
+      darkTheme: ThemeData(
+          brightness: Brightness.dark,
+          primarySwatch: MyColors.navy,
+          scaffoldBackgroundColor: const Color(0xfffcfcfc),
+          appBarTheme: const AppBarTheme(
+              backgroundColor: Colors.white,
+              elevation: .2,
+              titleTextStyle:
+                  TextStyle(fontFamily: Bold, color: Color(0xff323232)),
+              iconTheme: IconThemeData(color: MyColors.primaryColor))),
+      home: const SplashScreen(),
+    );
+  }
+}
